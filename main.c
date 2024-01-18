7445d3f35bed9daaca378bd9dcee89e2393a7c97
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2024-01-18 15:49:19 -0300

    Moving around the plane

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define SAMPLE_RATE 1.0
+#define ASPECT_RATIO (16.0f / 9.0f)
 
 Vector2 FlipY(Vector2 v, int height) {
   return (Vector2){
@@ -16,7 +16,8 @@ Vector2 FlipY(Vector2 v, int height) {
 
 int main(void) {
   const int screenWidth = 1920;
-  const int screenHeight = 1080;
+  // const int screenWidth = 960;
+  const int screenHeight = screenWidth / ASPECT_RATIO;
 
   InitWindow(screenWidth, screenHeight, "y = x^2 Plot");
 
@@ -27,7 +28,7 @@ int main(void) {
 
   double zoom = 100;
 
-  Vector2 center = {
+  Vector2 screen_center = {
       .x = (double)screenWidth / 2,
       .y = (double)screenHeight / 2,
   };
@@ -41,6 +42,11 @@ int main(void) {
     BeginDrawing();
     ClearBackground(BLACK);
 
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+      Vector2 d = GetMouseDelta();
+      screen_center = Vector2Add(screen_center, d);
+    }
+
     float f;
     if ((f = GetMouseWheelMove()) != 0) {
       zoom += f * 10;
@@ -49,51 +55,60 @@ int main(void) {
       }
     }
 
-    DrawRectangle(screenWidth / 2, 0, 2, screenHeight, WHITE);
-    DrawRectangle(0, screenHeight / 2, screenWidth, 2, WHITE);
+    DrawRectangle(screen_center.x, 0, 2, screenHeight, WHITE);
+    DrawRectangle(0, screen_center.y, screenWidth, 2, WHITE);
 
-    int x_segments = screenWidth / zoom / 2 + 1;
-    int y_segments = screenHeight / zoom / 2 + 1;
+    int x_segments = screen_center.x / zoom + 1;
+    int y_segments = screen_center.y / zoom + 1;
 
+    // TODO: Draw more or less lines depending on zoom
+    // TODO: Move lines with the camera
     for (int i = 0; i < x_segments; i++) {
-      int x1 = i * zoom + center.x;
+      int x1 = i * zoom + screen_center.x;
       DrawLine(x1, 0, x1, screenHeight, strip_color);
 
-      int x2 = center.x - i * zoom;
+      int x2 = screen_center.x - i * zoom;
       DrawLine(x2, 0, x2, screenHeight, strip_color);
 
-      DrawText(TextFormat("%d", i), x1 + 5, screenHeight / 2 + 5, 12, WHITE);
+      DrawText(TextFormat("%d", i), x1 + 5, screen_center.y + 5, 12, WHITE);
       if (i != 0) {
-        DrawText(TextFormat("-%d", i), x2 + 5, screenHeight / 2 + 5, 12, WHITE);
+        DrawText(TextFormat("-%d", i), x2 + 5, screen_center.y + 5, 12, WHITE);
       }
     }
 
     for (int i = 0; i < y_segments; i++) {
-      int y1 = i * zoom + center.y;
+      int y1 = i * zoom + screen_center.y;
       DrawLine(0, y1, screenWidth, y1, strip_color);
 
-      int y2 = center.y - i * zoom;
+      int y2 = screen_center.y - i * zoom;
       DrawLine(0, y2, screenWidth, y2, strip_color);
 
-      DrawText(TextFormat("%d", i), screenWidth / 2, y1, 12, WHITE);
       if (i != 0) {
-        DrawText(TextFormat("-%d", i), screenWidth / 2 + 5, y2 + 5, 12, WHITE);
+        float offset = 5;
+        DrawText(TextFormat("%d", i), screen_center.x + offset, y2 - offset, 12,
+                 WHITE);
+        DrawText(TextFormat("-%d", i), screen_center.x + offset, y1 - offset,
+                 12, WHITE);
       }
     }
 
     Vector2 *samples = calloc(bufsize, sizeof(Vector2));
 
     int i = 0;
-    for (double x = (float)-screenWidth / 2; x < (float)screenWidth / 2;
+    for (double x = -screen_center.x; x < screen_center.x;
          x += SAMPLE_RATE, i++) {
-      double y = -powf(x, 2) / zoom;
-      Vector2 current_point = Vector2Add(center, (Vector2){x, y});
-      samples[i] = FlipY(current_point, screenHeight);
+      double y = powf(x, 2) / zoom;
+
+      // y is inverted because of the way the screen coordinates work
+      Vector2 current_point = Vector2Add(screen_center, (Vector2){x, -y});
+      samples[i] = current_point;
+
+      // Linear interpolation between sample points
+      if (i > 0) {
+        Vector2 prev_point = samples[i - 1];
+        DrawLineEx(current_point, prev_point, 2.0f, RED);
+      }
     }
-
-    DrawSplineBezierQuadratic(samples, bufsize, 2, RED);
-
-    DrawText(TextFormat("%f", zoom), 0, 0, 16, RED);
 
     EndDrawing();
   }
